b3c0f57b59df58417fbe07d5374ef30fedcd8732
pond <pond@localhost>
2011-12-08 00:00:00 +0700

    ????????????? face recog ????????????????
    //?????
    	???????????????????????????????
    	ui????????????????

--- pocketsphinx/nodes/voice_cmd_vel.py
+++ pocketsphinx/nodes/voice_cmd_vel.py
@@ -15,6 +15,10 @@ import os
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
+set_init = ['lumyai']
+set_name = ['pond','bank','max','ohm','pee']
+
+
 class voice_cmd_vel:
 
     def __init__(self):
@@ -26,7 +30,8 @@ class voice_cmd_vel:
 	self.pubWhoiswho = rospy.Publisher('cmd_Whoiswho', String)
 	self.pubFollowme = rospy.Publisher('cmd_Followme', String)
 	self.pubManipulation = rospy.Publisher('cmd_Followme', String)
-
+	self.state = 1
+	self.name = ''
 	#Subscribe
 	rospy.Subscriber('recognizer/output', String, self.checkstate)
 
@@ -42,117 +47,60 @@ class voice_cmd_vel:
 	os.system("espeak --stdout \'" + sp + "' | aplay")
 
     def checkstate(self, msg):
-        rospy.loginfo(msg.data)
-	if msg.data.find("hello") > -1:
-		self.fspeak("hello")
-	if (self.Currentstate[0]==0):
-		if msg.data.find("navigation test") > -1:
-			self.Currentstate[0]=1
-			self.fspeak("this is navigation test")
-		elif msg.data.find("who is who test") > -1:
-			self.Currentstate[0]=2
-			self.fspeak("this is who is who test")
-		elif msg.data.find("follow me test") > -1:
-			self.Currentstate[0]=3
-			self.fspeak("this is follow me test")
-		elif msg.data.find("manipulation test") > -1:
-			self.Currentstate[0]=4
-			self.fspeak("this is manipulation test")
-	elif (self.Currentstate[0]==1):
-		if msg.data.find("target is one") > -1:
-			self.Currentstate[1]=1
-			self.fspeak("target is one")
-		elif msg.data.find("target is two") > -1:
-			self.Currentstate[1]=2
-			self.fspeak("target is two")
-		elif msg.data.find("target is three") > -1:
-			self.Currentstate[1]=3
-			self.fspeak("target is three")
-		self.pubNavigation.publish(self.Currentstate[1])
-	elif (self.Currentstate[0]==2):
-		if msg.data.find("my name is richard") > -1:
-			self.Currentstate[1]=1
-			self.fspeak("my name is richard")
-		elif msg.data.find("you are phillip") > -1:
-			self.Currentstate[1]=2
-			self.fspeak("you are is phillip")
-		elif msg.data.find("my name is emma") > -1:
-			self.Currentstate[1]=3
-			self.fspeak("you are is emma")
-		elif msg.data.find("my name is daniel") > -1:
-			self.Currentstate[1]=4
-			self.fspeak("you are is daniel")
-		elif msg.data.find("my name is tina") > -1:
-			self.Currentstate[1]=5
-			self.fspeak("you are is tina")
-		elif msg.data.find("my name is steve") > -1:
-			self.Currentstate[1]=6
-			self.fspeak("you are is steve")
-		elif msg.data.find("my name is henry") > -1:
-			self.Currentstate[1]=7
-			self.fspeak("you are is henry")
-		elif msg.data.find("my name is peter") > -1:
-			self.Currentstate[1]=8
-			self.fspeak("you are is peter")
-		elif msg.data.find("my name is robert") > -1:
-			self.Currentstate[1]=9
-			self.fspeak("you are is robert")
-		elif msg.data.find("my name is sarah") > -1:
-			self.Currentstate[1]=10
-			self.fspeak("you are is sarah")
-		elif msg.data.find("my name is brian") > -1:
-			self.Currentstate[1]=11
-			self.fspeak("you are is brian")
-		elif msg.data.find("my name is thomas") > -1:
-			self.Currentstate[1]=12
-			self.fspeak("you are is thomas")
-		elif msg.data.find("my name is britney") > -1:
-			self.Currentstate[1]=13
-			self.fspeak("you are is britney")
-		elif msg.data.find("my name is justin") > -1:
-			self.Currentstate[1]=14
-			self.fspeak("you are is justin")
-		elif msg.data.find("my name is tony") > -1:
-			self.Currentstate[1]=15
-			self.fspeak("you are is tony")
-		elif msg.data.find("my name is kevin") > -1:
-			self.Currentstate[1]=16
-			self.fspeak("you are is kevin")
-		elif msg.data.find("my name is joseph") > -1:
-			self.Currentstate[1]=17
-			self.fspeak("you are is joseph")
-		elif msg.data.find("my name is michael") > -1:
-			self.Currentstate[1]=18
-			self.fspeak("you are is michael")
-		elif msg.data.find("my name is michelle") > -1:
-			self.Currentstate[1]=19
-			self.fspeak("you are is michelle")
-		elif msg.data.find("my name is donna") > -1:
-			self.Currentstate[1]=20
-			self.fspeak("you are is donna")
-		self.pubWhoiswho.publish(self.Currentstate[1])
-	elif (self.Currentstate[0]==3):
-		if msg.data.find("follow me") > -1:
-			self.Currentstate[1]=1
-			self.fspeak("i will follow you")
-		elif msg.data.find("wait here") > -1:
-			self.Currentstate[1]=2
-			self.fspeak("i am waiting for you")
-		self.pubFollowme.publish(self.Currentstate[1])
-	elif (self.Currentstate[0]==4):
-		if msg.data.find("manipulation") > -1:
-			self.Currentstate[1]=1
-			self.fspeak("i will manipulate")
-		self.pubManipulation.publish(self.Currentstate[1])
-	if msg.data.find("reset") > -1:
-		self.Currentstate[1]=0
-		self.Currentstate[0]=0
-		self.fspeak("the state is reseted")
-
+        rospy.loginfo(msg.data+' '+str(self.state))
 	#self.pub_.publish(self.Currentstate[1]*10+self.Currentstate[0])
-	self.pub_.publish(msg)
-
-  
+	msg = str(msg.data)
+	if(msg in set_init):
+		state = 1
+	if(self.state == 1):
+		if(msg == 'remember me'):
+			self.fspeak('command is '+msg+' yes or no')
+			self.state = 2
+		elif(msg == 'test'):
+			self.fspeak('command is '+msg+' yes or no')
+			self.state = 5
+		elif(msg == 'target'):
+			self.fspeak('command is '+msg+' yes or no')
+			self.state = 6
+		else:
+			self.state = 1
+	elif(self.state == 5 or self.state == 6 ):
+		if(msg == 'yes'):	
+			self.fspeak('please wait')
+			if(self.state == 5):
+				self.pub_.publish('test')
+			else:
+				self.pub_.publish('target')
+			self.state = 1
+		elif(msg == 'no'):
+			self.state = 1
+		else:
+			self.fspeak('yes or no')
+	elif(self.state == 2):
+		if(msg == 'yes'):
+			self.fspeak('what is your name')
+			self.state = 3
+		elif(msg == 'no'):
+			self.state = 1
+		else:
+			self.fspeak('yes or no')
+	elif(self.state == 3):
+		self.name = msg
+		self.fspeak('your name is '+msg+' yes or no')
+		self.state = 4
+	elif(self.state == 4):
+		if(msg == 'yes'):	
+			self.fspeak('please wait')
+			self.pub_.publish(self.name)
+			self.state = 1
+		elif(msg == 'no'):
+			self.fspeak('what is your name')
+			self.state = 3
+		else:
+			self.fspeak('yes or no')
+	rospy.loginfo('now state is'+str(self.state))
+			
+		
 if __name__=="__main__":
     rospy.init_node('voice_cmd_vel')
     try:
