18ccf91fc051b2aefa3a3e8536264257d650d4da
new <new@localhost>
2016-01-16 15:29:27 +0700

    Move files to same directory.

---
+++ voice_recog/resturant_Frank/match.py
@@ -0,0 +1,11 @@
+__author__ = 'Frank'
+
+a = ['coconut cereals', 'coco balls', 'egg stars', 'gram soup', 'bean sauce','apple', 'lemon', 'pear']
+# a = ['green tea', 'papaya milk', 'pure milk', 'water', 'orange juice', 'beer']
+for i in a:
+    for j in a:
+        if i != j:
+            print 'Get me',i , 'and',j
+
+# for i in a:
+    # print i
