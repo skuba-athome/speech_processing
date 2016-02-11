2d864c66ec0b5bbf99b40331b2d71f0f4950a1c7
Nathas Yingsukamol <new08242@gmail.com>
2016-02-11 22:29:51 +0700

    Swith grammar and LM

--- nodes/recognizer_grammar.py
+++ nodes/recognizer_grammar.py
@@ -26,8 +26,13 @@ gobject.threads_init()
 import gst
 
 from std_msgs.msg import String
-from std_srvs.srv import *
+# from std_srvs.srv import Trigger
+from speech_processing.srv import *
 import sys
+
+import actionlib
+import speech_processing.msg
+from include import gram_lm
 
 class grammar_recognizer(object):
     """ GStreamer based speech recognizer. """
@@ -39,18 +44,29 @@ class grammar_recognizer(object):
         rospy.on_shutdown(self.shutdown)
 
         # services to start/stop recognition
-        rospy.Service("~start", Empty, self.start)
-        rospy.Service("~stop", Empty, self.stop)
+        # rospy.Service("~start", Trigger, self.start)
+        # rospy.Service("~stop", Trigger, self.stop)
 
         # configure pipeline
-        self.pipeline = gst.parse_launch('gconfaudiosrc ! audioconvert ! audioresample '
-                                         + '! vader name=vad auto-threshold=true '
-                                         + '! pocketsphinx name=asr ! fakesink')
-        asr = self.pipeline.get_by_name('asr')
-        asr.connect('partial_result', self.asr_partial_result)
-        asr.connect('result', self.asr_result)
-        asr.set_property('configured', True)
-        asr.set_property('dsratio', 1)
+        self.pipeline_gram = gst.parse_launch('gconfaudiosrc ! audioconvert ! audioresample '
+                                         + '! vader name=vad_gram auto-threshold=true '
+                                         + '! pocketsphinx name=asr_gram ! fakesink')
+        asr_gram = self.pipeline_gram.get_by_name('asr_gram')
+        asr_gram.connect('partial_result', self.asr_partial_result)
+        asr_gram.connect('result', self.asr_result)
+        asr_gram.set_property('configured', True)
+        asr_gram.set_property('dsratio', 1)
+        self.vader_gram = self.pipeline_gram.get_by_name('vad_gram')
+
+        self.pipeline_lm = gst.parse_launch('gconfaudiosrc ! audioconvert ! audioresample '
+                                         + '! vader name=vad_lm auto-threshold=true '
+                                         + '! pocketsphinx name=asr_lm ! fakesink')
+        asr_lm = self.pipeline_lm.get_by_name('asr_lm')
+        asr_lm.connect('partial_result', self.asr_partial_result)
+        asr_lm.connect('result', self.asr_result)
+        asr_lm.set_property('configured', True)
+        asr_lm.set_property('dsratio', 1)
+        self.vader_lm = self.pipeline_lm.get_by_name('vad_lm')
 
         # parameters for lm and dic
         try:
@@ -64,30 +80,166 @@ class grammar_recognizer(object):
             rospy.logerr('Please specify a dictionary')
             return
 
-        asr.set_property('fsg',self.grammar_)
-        asr.set_property('dict',self.dict_)
+        # asr.set_property('fsg',self.grammar_)
+        # asr.set_property('dict',self.dict_)
 
-        bus = self.pipeline.get_bus()
-        bus.add_signal_watch()
-        bus.connect('message::application', self.application_message)
-        self.start(None)
+        bus_gram = self.pipeline_gram.get_bus()
+        bus_gram.add_signal_watch()
+        bus_gram.connect('message::application', self.application_message)
+
+        bus_lm = self.pipeline_lm.get_bus()
+        bus_lm.add_signal_watch()
+        bus_lm.connect('message::application', self.application_message)
+        # self.start(None)
+
+        self.ACTION_NAME = "~voice_reg"
+        self.result = speech_processing.msg.VoiceRegResult()
+        self.action_server = actionlib.SimpleActionServer(self.ACTION_NAME, speech_processing.msg.
+            VoiceRegAction, execute_cb = self.execute_cb, auto_start = False)
+        self.action_server.start()
+
+        self.gramLm = gram_lm.GramLm
+        # self.stop()
+        # self.grammar_ = '/home/skuba/obsidian_workspace/src/speech_processing/dic/5questions_and_follow_New/start.fsg'
+        # self.dict_ = '/home/skuba/obsidian_workspace/src/speech_processing/dic/5questions_and_follow_New/start.dic'
+        # self.start(None)
+
         gtk.main()
-        
+
+    def execute_cb(self, goal):
+        """ Callback function run everytime when new goal recieve """
+
+        if goal.grammar_id == -1 :
+
+            self.pipeline_gram.set_state(gst.STATE_NULL)
+            self.pipeline_lm.set_state(gst.STATE_NULL)
+            self.vader_gram.set_property('silent', True)
+            self.vader_lm.set_property('silent', True)
+
+            rospy.loginfo('%s: Succeeded Stop Both' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -2 :
+
+            self.pipeline_gram.set_state(gst.STATE_NULL)
+            self.vader_gram.set_property('silent', True)
+
+            rospy.loginfo('%s: Succeeded Stop Gram' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -3 :
+
+            self.pipeline_lm.set_state(gst.STATE_NULL)
+            self.vader_lm.set_property('silent', True)
+
+            rospy.loginfo('%s: Succeeded Stop LM' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -4 :
+
+            self.pipeline_gram.set_state(gst.STATE_PAUSED)
+            self.pipeline_lm.set_state(gst.STATE_PAUSED)
+            self.vader_gram.set_property('silent', True)
+            self.vader_lm.set_property('silent', True)
+
+            rospy.loginfo('%s: Succeeded Stop Both' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -5 :
+
+            self.pipeline_gram.set_state(gst.STATE_PAUSED)
+            self.vader_gram.set_property('silent', True)
+
+            rospy.loginfo('%s: Succeeded Stop Gram' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -6 :
+
+            self.pipeline_lm.set_state(gst.STATE_PAUSED)
+            self.vader_lm.set_property('silent', True)
+
+            rospy.loginfo('%s: Succeeded Stop LM' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -7 :
+
+            self.vader_gram.set_property('silent', False)
+            self.vader_lm.set_property('silent', False)
+            self.pipeline_gram.set_state(gst.STATE_PLAYING)
+            self.pipeline_lm.set_state(gst.STATE_PLAYING)
+
+            rospy.loginfo('%s: Succeeded Stop Both' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -8 :
+
+            self.vader_lm.set_property('silent', False)
+            self.pipeline_gram.set_state(gst.STATE_PLAYING)
+
+            rospy.loginfo('%s: Succeeded Stop Gram' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        elif goal.grammar_id == -9 :
+
+            self.vader_lm.set_property('silent', False)
+            self.pipeline_lm.set_state(gst.STATE_PLAYING)
+
+            rospy.loginfo('%s: Succeeded Stop LM' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)        
+
+        elif goal.grammar_id < 100 :
+            
+            self.vader_gram.set_property('silent', True)
+            self.vader_lm.set_property('silent', True)
+            self.pipeline_gram.set_state(gst.STATE_NULL)
+            self.pipeline_gram.set_state(gst.STATE_READY)
+
+            self.grammar_ = self.gramLm.gram[goal.grammar_id]
+            self.dict_ = self.gramLm.dic[goal.grammar_id]
+
+            self.pipeline_gram.get_by_name('asr_gram').set_property('fsg',self.grammar_)
+            self.pipeline_gram.get_by_name('asr_gram').set_property('dict',self.dict_)
+            # rospy.loginfo(self.pipeline_gram.get_by_name('asr_gram').get_property('fsg'))
+            # rospy.loginfo(self.pipeline_lm.get_by_name('asr_lm').get_property('lm'))
+            self.pipeline_gram.set_state(gst.STATE_PLAYING)
+
+            rospy.loginfo('%s: Succeeded Gram' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
+        else :
+
+            self.vader_gram.set_property('silent', True)
+            self.vader_lm.set_property('silent', True)
+            self.pipeline_lm.set_state(gst.STATE_NULL)
+            self.pipeline_lm.set_state(gst.STATE_READY)
+
+            self.grammar_ = self.gramLm.gram[goal.grammar_id]
+            self.dict_ = self.gramLm.dic[goal.grammar_id-100]
+
+            self.pipeline_lm.get_by_name('asr_lm').set_property('lm',self.grammar_)
+            self.pipeline_lm.get_by_name('asr_lm').set_property('dict',self.dict_)
+            # rospy.loginfo(self.pipeline_gram.get_by_name('asr_gram').get_property('fsg'))
+            # rospy.loginfo(self.pipeline_lm.get_by_name('asr_lm').get_property('lm'))
+            self.pipeline_lm.set_state(gst.STATE_PLAYING)
+
+            rospy.loginfo('%s: Succeeded LM' % self.ACTION_NAME)
+            self.action_server.set_succeeded(self.result)
+
     def shutdown(self):
         """ Shutdown the GTK thread. """
         gtk.main_quit()
 
-    def start(self, msg):
-        self.pipeline.set_state(gst.STATE_PLAYING)
-        self.pipeline.get_by_name('asr').set_property('fsg',self.grammar_)
-        self.pipeline.get_by_name('asr').set_property('dict',self.dict_)
-        return EmptyResponse()
+    # def start(self, msg):
+    #     self.pipeline.set_state(gst.STATE_PLAYING)
+    #     self.pipeline.get_by_name('asr').set_property('fsg',self.grammar_)
+    #     self.pipeline.get_by_name('asr').set_property('dict',self.dict_)
+    #     return TriggerResponse(True, 'success')
 
-    def stop(self):
-        self.pipeline.set_state(gst.STATE_PAUSED)
-        #vader = self.pipeline.get_by_name('vad')
-        #vader.set_property('silent', True)
-        return EmptyResponse()
+    # def stop(self, msg, num):
+    #     self.pipeline.set_state(gst.STATE_PAUSED)
+    #     #self.vader = self.pipeline.get_by_name('vad')
+    #     #self.vader.set_property('silent', True)
+    #     return TriggerResponse(True, 'success')
 
     def asr_partial_result(self, asr, text, uttid):
         """ Forward partial result signals on the bus to the main thread. """
