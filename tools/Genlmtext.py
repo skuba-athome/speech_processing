f8989e2e358aa5ae045ef3cdd323f0b287346840
Nathas Yingsukamol <new08242@gmail.com>
2016-03-24 13:11:18 +0700

    cafe au lait

--- tools/Genlmtext.py
+++ tools/Genlmtext.py
@@ -4,19 +4,26 @@ class GenLm(object):
 	"""docstring for GenLm"""
 	def __init__(self):
 		super(GenLm, self).__init__()
-		self.head = ["I want", "I will take"]
-		self.conj = ["and", "with"]
+		self.head = ["go", "navigate"]
+		self.conj = ["to"]
 		self.food = ["noodles", "peanuts", "hamburger", "fries"]
-		self.drink = ["orange juice", "water"]
+		self.place = ["kitchen" , "living room" , "bedroom" , "hallway" , "bathroom"]
+		# self.drink = ["orange juice", "water"]
 
 	def gen(self):
 		for head in self.head:
 			for conj in self.conj:
-				for food in self.food:
-					print head + " " + conj + " " + food
+				# Food
+				# for food in self.food:
+				# 	print head + " " + conj + " " + food
 
-			for drink in self.drink:
-				print head + " " + drink
+				# Place
+				for place in self.place:
+					print head + " " + conj + " " + place
+
+			# Drink
+			# for drink in self.drink:
+			# 	print head + " " + drink
 			
 
 if __name__=="__main__":
