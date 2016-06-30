29c248f1474a3aeb572aeeecf39bf0cfc713489c
nutkaewnak <nut-boss@hotmail.com>
2016-06-30 02:56:19 +0200

    fix bug speech

--- nodes/recognizer_grammar_gst1.py
+++ nodes/recognizer_grammar_gst1.py
@@ -46,14 +46,21 @@ import gtk
 from std_msgs.msg import String
 from std_srvs.srv import *
 import sys
+import csv
+import os
+import rospkg
 
 class grammar_recognizer(object):
     """ GStreamer based speech recognizer. """
 
     def __init__(self):
         """ Initialize the speech pipeline components. """
+        rospack = rospkg.RosPack()
+        self.ROS_PATH = rospack.get_path('speech_processing')
+
         rospy.init_node('recognizer_grammar')
         self.pub = rospy.Publisher('~output',String)
+        rospy.Subscriber('/voice/output', String, self.write_command_file)
         rospy.on_shutdown(self.shutdown)
 
         # services to start/stop recognition
@@ -64,19 +71,25 @@ class grammar_recognizer(object):
         self.start_record = False
         self.timestamp = '{:%Y-%m-%d_%H:%M:%S}'.format(datetime.datetime.now())
         self.pipeline_rec = gst.parse_launch('autoaudiosrc name=audiosrc ! audioconvert ! audioresample ! wavenc ' +
-                                             '! filesink name=filesink location=/home/ketamine/skuba_ws/src' +
-                                             '/speech_processing/SKUBA_ShotgunMic_' +
-                                             self.timestamp + '.wav')
+                                             '! filesink name=filesink location=' + self.ROS_PATH +
+                                             'SKUBA_ShotgunMic_' + self.timestamp + '.wav')
 
-        self.pipeline = gst.parse_launch('autoaudiosrc ! audioconvert ! audioresample '
-                                         + '! pocketsphinx  name=asr ! fakesink ')
+        self.pipeline = gst.parse_launch('autoaudiosrc ! audioconvert ! audioresample ! volume name=volume '
+                                         + '! pocketsphinx name=asr ! fakesink')
 
         # audiosrc = self.pipeline_rec.get_by_name('audiosrc')
         self.filesink = self.pipeline_rec.get_by_name('filesink')
 
-
+        self.command_filename = self.ROS_PATH + 'SKUBA_ShotgunMic_' + self.timestamp + '.csv'
+        self.command_timestamp = None
+        self.csvfile = open(self.command_filename, 'w')
+        fieldnames = ['TimeStamp', 'Command']
+        self.writer = csv.DictWriter(self.csvfile, fieldnames=fieldnames)
+        self.writer.writeheader()
 
         asr = self.pipeline.get_by_name('asr')
+        self.volume = self.pipeline.get_by_name('volume')
+        # self.volume.set_property('mute', True)
 
         # parameters for lm and dic
         try:
@@ -98,7 +111,10 @@ class grammar_recognizer(object):
         bus.add_signal_watch()
         bus.connect('message::element', self.element_message)
 
+        self.record_pipeline()
 
+        service_open = rospy.Service('~mic_control_open', Empty, self.microphone_open)
+        service_close = rospy.Service('~mic_control_close', Empty, self.microphone_close)
         # bus_rec = self.pipeline_rec.get_bus()
         # bus_rec.add_signal_watch()
         # bus_rec.connect('message::element', self.element_message)
@@ -144,16 +160,16 @@ class grammar_recognizer(object):
 
     def element_message(self, bus, msg):
         """Receive element messages from the bus."""
-        print type(msg)
+        # print type(msg)
         msgtype = msg.get_structure().get_name()
         if msgtype != 'pocketsphinx':
             return
 
         if msg.get_structure().get_value('final'):
             self.final_result(msg.get_structure().get_value('hypothesis'), msg.get_structure().get_value('confidence'))
-            self.pipeline_rec.change_state(gst.StateChange.PLAYING_TO_PAUSED)
-            self.pipeline_rec.change_state(gst.StateChange.PAUSED_TO_READY)
-            self.pipeline_rec.change_state(gst.StateChange.READY_TO_NULL)
+            # self.pipeline_rec.change_state(gst.StateChange.PLAYING_TO_PAUSED)
+            # self.pipeline_rec.change_state(gst.StateChange.PAUSED_TO_READY)
+            # self.pipeline_rec.change_state(gst.StateChange.READY_TO_NULL)
             self.start_record = False
             # try:
             #     self.pipeline_rec.set_state(gst.State.NULL)
@@ -169,7 +185,8 @@ class grammar_recognizer(object):
             # self.pipeline.set_state(gst.State.PLAYING)
             if self.start_record == False:
                 self.start_record = True
-                self.record_pipeline()
+                # self.record_pipeline()
+                self.create_command_file()
             # self.pipeline_rec.change_state(gst.StateChange.PAUSED_TO_PLAYING)
             # print self.filesink.get_property('ts-offset'), "-------------------------------------"
             self.partial_result(msg.get_structure().get_value('hypothesis'))
@@ -178,8 +195,7 @@ class grammar_recognizer(object):
         self.timestamp = '{:%Y-%m-%d_%H:%M:%S}'.format(datetime.datetime.now())
         print 'record--------------', self.timestamp
 
-        self.filesink.set_property('location', '/home/ketamine/skuba_ws/src' +
-                                   '/speech_processing/SKUBA_ShotgunMic_' +
+        self.filesink.set_property('location', self.ROS_PATH + 'SKUBA_ShotgunMic_' +
                                    self.timestamp + '.wav')
 
         # self.pipeline_rec = gst.parse_launch('autoaudiosrc name=audiosrc ! audioconvert ! audioresample ! wavenc ' +
@@ -197,6 +213,28 @@ class grammar_recognizer(object):
         self.pipeline_rec.change_state(gst.StateChange.READY_TO_PAUSED)
         self.pipeline_rec.change_state(gst.StateChange.PAUSED_TO_PLAYING)
 
+    def create_command_file(self):
+        if self.command_timestamp == None:
+            self.command_timestamp = '{:%Y-%m-%d_%H:%M:%S}'.format(datetime.datetime.now())
+
+    def write_command_file(self, data):
+        print 'write file====================================================='
+        if self.command_timestamp == None:
+            self.command_timestamp = '{:%Y-%m-%d_%H:%M:%S}'.format(datetime.datetime.now())
+
+        self.writer.writerow({'TimeStamp': self.command_timestamp, 'Command': data.data})
+        # self.command_filename = None
+        self.command_timestamp = None
+
+    def microphone_open(self, data):
+        print 'open==========================='
+        self.volume.set_property('mute', False)
+        return EmptyResponse()
+
+    def microphone_close(self, data):
+        print  'close========================='
+        self.volume.set_property('mute', True)
+        return EmptyResponse()
+
 if __name__=="__main__":
     r = grammar_recognizer()
-
